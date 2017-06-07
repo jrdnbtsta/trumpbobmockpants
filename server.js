019bd5e06587a3f3117609ff2e7872afe1144f7d
Jordan Bautista <jordanbau@gmail.com>
2017-06-07 10:20:09 -0700

    comments

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 const Twit = require('twit');
 const fs = require('fs');
 const keys = require('./accessKeys.js');
-
 const T = new Twit(keys);
 
+//function that takes a string, capitalizes every third and fifth letter
 function randomCap(word) {
 	return word.split('').reduce((all, item, index) => {
 		if (index === 0) item = item.toLowerCase();
@@ -17,37 +17,39 @@ function randomCap(word) {
 
 lastTweet = {};
 
+//interval GET req to check last tweets. o0nce a min because of twitter's API rate limit
 setInterval(() => {
 	T.get('statuses/user_timeline', { user_id: 25073877, count: 1 }, (err, data0, response) => {
 		if (err) console.log(err);
 
+		//if there's a new tweet ...
 		if (data0[0].id !== lastTweet.id) {
 			let newText = '@' + data0[0].user.screen_name + ' ' + randomCap(data0[0].text);
 			if(newText.length > 140) newText = newText.substr(0,140);
 
+			//upload pic
 			let b64content = fs.readFileSync('/Users/jordanb/Desktop/projects/trumpbobmockpants/media/trumpBot.png', { encoding: 'base64' })
 
-			T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+			T.post('media/upload', { media_data: b64content }, (err, data, response) => {
 
 				let mediaIdStr = data.media_id_string
 				let altText = 'SpongeBob MockPants'
 				let meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
 
-				T.post('media/metadata/create', meta_params, function (err, data, response) {
+				T.post('media/metadata/create', meta_params, (err, data, response) => {
 					if (!err) {
+
 						let params = { status: newText, in_reply_to_status_id: data0[0].id_str, media_ids: [mediaIdStr] }
 						
-						console.log('text: ', newText, ' len: ', newText.length);
-						T.post('statuses/update', params, function (err, data, response) {
-							console.log('replied to Text: ', data);
+						T.post('statuses/update', params, (err, data, response) => {
+							console.log('replied to new tweet!');
 						})
 					}
-				})
-			})
-
+				});
+			});
 			lastTweet = data0[0];
 		}
 	})
-}, 1000 * 5);
+}, 1000 * 60);
 
-app.listen(3000, () => console.log('connected to server 3000'));
+app.listen(8080, () => console.log('connected to server 8080'));
